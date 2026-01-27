3d3b0f7316b568fea26c00cda95e27fda9643dfa
biertisch <almeidarodrigues.beatriz@gmail.com>
2026-01-27 13:52:37 +0000

    first version

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,57 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Vogon"), _grade(150) {}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+	: _name(name), _grade(grade) {
+
+		if (grade < 1)
+			throw GradeTooHighException();
+		else if (grade > 150)
+			throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+	: _name(other._name), _grade(other._grade) {}
+
+Bureaucrat::~Bureaucrat() {}
+
+// Assignment only copies grade (name is const)
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+	if (this != &other)
+		_grade = other._grade;
+	return *this;
+}
+
+const std::string& Bureaucrat::getName() const {
+	return _name;
+}
+
+int Bureaucrat::getGrade() const {
+	return _grade;
+}
+
+void Bureaucrat::incrementGrade(int nbIncrement) {
+	if (_grade - nbIncrement < 1)
+		throw GradeTooHighException();
+	_grade -= nbIncrement;
+}
+
+void Bureaucrat::decrementGrade(int nbDecrement) {
+	if (_grade + nbDecrement > 150)
+		throw GradeTooLowException();
+	_grade += nbDecrement;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return "Bureaucrat grade too high";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return "Bureaucrat grade too low";
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b) {
+	os << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+	return os;
+}
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,62 @@
+#include "Bureaucrat.hpp"
+
+void printSection(const std::string& title)
+{
+	int width = 50;
+	int padding = (width - title.size()) / 2;
+	std::cout << '\n' << std::string(padding, ' ') << title	<< std::string(padding, ' ') << '\n';
+	std::cout << std::string(width, '=') << '\n';
+}
+
+void alterTryCatch(Bureaucrat *vogon, void (Bureaucrat::*func)(int n), int nb = 1)
+{
+	if (!vogon)
+		return;
+
+	try {
+		(vogon->*func)(nb);
+		std::cout << *vogon << '\n';
+	}
+
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+Bureaucrat *createTryCatch(const std::string& name, int grade) {
+
+	try {
+		Bureaucrat *vogon = new Bureaucrat(name, grade);
+		std::cout << *vogon << '\n';
+		return vogon;
+	}
+
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return NULL;
+	}
+}
+
+int main() {
+
+	printSection("CREATING BUREAUCRATS");
+	Bureaucrat *a = createTryCatch("a", 1);
+	Bureaucrat *b = createTryCatch("b", 150);
+	Bureaucrat *c = createTryCatch("c", 0); // grade too high
+	Bureaucrat *d = createTryCatch("d", 151); // grade too low
+
+	printSection("INCREMENTING & DECREMENTING GRADES");
+	alterTryCatch(a, &Bureaucrat::decrementGrade);
+	alterTryCatch(a, &Bureaucrat::incrementGrade);
+	alterTryCatch(a, &Bureaucrat::incrementGrade); // grade too high
+	alterTryCatch(b, &Bureaucrat::incrementGrade, 5);
+	alterTryCatch(b, &Bureaucrat::decrementGrade, 50); // grade too low
+
+	// free memory
+	delete a;
+	delete b;
+	delete c;
+	delete d;
+
+	return 0;
+}

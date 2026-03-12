e2d16b8e259410fe4150c35e52e3f340ba014c8e
biertisch <almeidarodrigues.beatriz@gmail.com>
2026-03-12 12:24:37 +0000

    minor performance adjustment

--- cpp07/ex01/include/iter.hpp
+++ cpp07/ex01/include/iter.hpp
@@ -6,6 +6,6 @@ void iter(T* array, const unsigned int length, F& function)
 	if (!array)
 		return;
 
-	for (unsigned int i = 0; i < length; i++)
+	for (unsigned int i = 0; i < length; ++i)
 		function(array[i]);
 }
